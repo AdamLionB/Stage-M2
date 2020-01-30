ae07bb4a1e2f7c54c61c76f99456134bb65174e0
AdamLionB <lion.adam.otman@outlook.fr>
2020-01-30 17:02:41 +0100

    true f1, weighted f1, scale test, symetry test

--- brouillon.py
+++ brouillon.py
@@ -9,10 +9,11 @@ import pathlib
 import os
 from time import time
 from copy import copy
-from typing import List, Set, Callable, Iterator, Tuple, Union, Optional, Dict, Any, Generic
-from itertools import zip_longest, cycle, combinations
+from typing import List, Set, Callable, Iterator, Tuple, Union, Optional, Dict, Any, Generic, TypeVar
+from itertools import zip_longest, cycle, combinations, chain, islice
 from functools import reduce, partial, singledispatchmethod
 from collections import defaultdict
+import math
 
 sys_lib.path.insert(1, '../scorch')
 from scorch.main import METRICS, clusters_from_json
@@ -33,11 +34,66 @@ def kaapa(k: Partition, r: Partition):
             1 - (((c_k * c_r) + (n_k * n_r)) / (c_k + n_k) ** 2))
 
 
+def true_r(gold: Partition, sys: Partition) -> float:
+    n = 0
+    for c_r in sys:
+        if c_r in gold:
+            n += 1
+    return n / len(sys)
+
+
+def true_p(gold: Partition, sys: Partition) -> float:
+    n = 0
+    for c_r in gold:
+        if c_r in sys:
+            n += 1
+    return n / len(gold)
+
+
+def true_f1(gold: Partition, sys: Partition) -> Tuple[float, float, float]:
+    """
+    Computes the true f1 for the (gold, sys) the true f1 is computed based on the set correctly resolved
+    """
+    r = true_r(gold, sys)
+    p = true_p(gold, sys)
+    return r, p, 2 * r * p / (r + p) if r + p != 0 else 0
+
+
+def weighted_r(gold: Partition, sys: Partition) -> float:
+    n = 0
+    nb_mentions = 0
+    for c_r in sys:
+        if c_r in gold:
+            n += len(c_r)
+        nb_mentions += len(c_r)
+    return n / nb_mentions
+
+
+def weighted_p(gold: Partition, sys: Partition) -> float:
+    n = 0
+    nb_mentions = 0
+    for c_r in gold:
+        if c_r in sys:
+            n += len(c_r)
+        nb_mentions += len(c_r)
+    return n / nb_mentions
+
+
+def weighted_f1(gold: Partition, sys: Partition) -> Tuple[float, float, float]:
+    """
+    Computes the weighted f1 for the (gold, sys) the true f1 is
+    computed based on the set correctly resolved weighted by their size
+    """
+    r = weighted_r(gold, sys)
+    p = weighted_p(gold, sys)
+    return r, p, 2 * r * p / (r + p) if r + p != 0 else 0
+
 # print(tp_c, tp_n, c_k, n_k, c_r, n_r)
 
-Partition = List[Set]
+T = TypeVar('T')
+Partition = List[Set[T]]
 """
-Alias for a list of sets
+Alias for a list of sets all composed of a same T type object.
 """
 
 
@@ -77,6 +133,7 @@ def random_partition(mentions: List, rng: Callable[[], float]) -> Partition:
         mentions = mentions[y:]
     return partitions
 
+
 # TODO beta distribution vraiment idéal ?
 def beta_partition(mentions: List, a: float, b: float) -> Partition:
     """
@@ -90,14 +147,14 @@ def entity_partition(mentions: List) -> Partition:
     """
     Return the partition constitued of an unique entity
     """
-    return [{m for m in mentions}] #random_partition(mentions, lambda: 1)
+    return [{m for m in mentions}]  # random_partition(mentions, lambda: 1)
 
 
 def singleton_partition(mentions: List) -> Partition:
     """
     Return the partition consitued of only singletons
     """
-    return [{m} for m in mentions] #random_partition(mentions, lambda: 1 / (len(mentions) + 1))
+    return [{m} for m in mentions]  # random_partition(mentions, lambda: 1 / (len(mentions) + 1))
 
 
 class SuchRandom:
@@ -120,7 +177,7 @@ def wow_partition(mentions: List) -> Partition:
     return cluster
 
 
-def get_mentions(partition: Partition) -> List:
+def get_mentions(partition: Partition) -> List[T]:
     """
     Return the list of all mentions in a Partition
     """
@@ -135,10 +192,17 @@ SK_METRICS = {
 }
 
 
+def sign(x: float):
+    if x > 0: return 1
+    if x < 0: return 1
+    return 0
+
+
 class Scores:
     """
     Dictionnary linking a str as key to a tuple of values
     """
+
     def __init__(self, dic: Dict[str, Tuple[float, ...]]):
         self.dic = dic
 
@@ -221,7 +285,18 @@ class Scores:
             squared_sum += scores ** 2
             count += 1
         return (regular_sum / count,
-                ((squared_sum * count - regular_sum ** 2)/ (count * (count - 1))) ** (1 / 2))
+                ((squared_sum * count - regular_sum ** 2) / (count * (count - 1))) ** (1 / 2))
+
+    def compare(self, other: Scores) -> Scores:
+        return Scores({
+            sk: tuple((sign(y - x) for x, y in zip(sv, ov)))
+            for (sk, sv), ov in zip(self.dic.items(), other.dic.values())
+        })
+
+    @staticmethod
+    def growth(scoress: Iterator[Scores]) -> Scores:
+        pass
+
 
 
 def to_tuple(e: Union[Any, Tuple[Any]]) -> Tuple[Any]:
@@ -245,7 +320,7 @@ def partition_to_sklearn_format(partition: Partition) -> List:
 
 
 # TODO purify
-def get_scores(gold: Partition, sys: Partition) -> Scores:
+def evaluate(gold: Partition, sys: Partition) -> Scores:
     """
     Computes metrics scores for a (gold, sys) and outputs it as a Scores
     """
@@ -253,10 +328,13 @@ def get_scores(gold: Partition, sys: Partition) -> Scores:
     for name, metric in METRICS.items():
         res[name] = to_tuple(metric(gold, sys))
     res['conll'] = to_tuple(scores.conll2012(gold, sys))
+    res['true'] = to_tuple(true_f1(gold, sys))
+    res['w'] = to_tuple(weighted_f1(gold, sys))
     gold = partition_to_sklearn_format(gold)
     sys = partition_to_sklearn_format(sys)
-    for name, metric in SK_METRICS.items():
-        res[name] = to_tuple(metric(gold, sys))
+    if len(gold) == len(sys):
+        for name, metric in SK_METRICS.items():
+            res[name] = to_tuple(metric(gold, sys))
     return Scores(res)
 
 
@@ -303,7 +381,6 @@ def introduce_randomness(partition: Partition):
     return partition
 
 
-
 def score_random_partitions(
         golds: Iterator[Partition],
         partition_generator: Callable[[List], Partition]
@@ -313,7 +390,7 @@ def score_random_partitions(
     """
     for n, k in enumerate(golds):
         syss: Iterator[Partition] = (partition_generator(get_mentions(k)) for _ in range(1))
-        yield Scores.average(map(lambda r: get_scores(k, r), syss))
+        yield Scores.average(map(lambda r: evaluate(k, r), syss))
 
 
 # start = time()
@@ -335,14 +412,83 @@ def golds_vs_singleton(golds: Iterator[Partition]) -> Scores:
     return Scores.average(score_random_partitions(golds, singleton_partition))
 
 
+# TODO comment that
+def duplicate_clusters(gold: Partition, sys: Partition) -> Tuple[Partition, Partition]:
+    mentions = list(set().union(get_mentions(gold), get_mentions(sys)))
+    new_mentions = {}
+    cast = type(mentions[0])
+    for m in mentions:
+        while (r := cast(randint(-1e6, 1e6))) in mentions or r in new_mentions: pass
+        new_mentions[m] = r
+    it = chain(iter(mentions), (new_mentions[m] for m in mentions))
+    new_gold = [{next(it) for _ in c} for c in chain(gold, gold)]
+    it = chain(iter(mentions), (new_mentions[m] for m in mentions))
+    new_sys = [{next(it) for _ in c} for c in chain(sys, sys)]
+    return new_gold, new_sys
 
-print(golds_vs_entity(iter_ancor()))
+
+def scale_test(gold, sys) -> Scores:
+    score_a = evaluate(gold, sys)
+    score_b = evaluate(*duplicate_clusters(gold, sys))
+    return score_a.compare(score_b)
+
+
+def symetry_test(gold, sys) -> Scores:
+    scores_a = evaluate(gold, sys)
+    scores_b = evaluate(sys, gold)
+    return scores_a.compare(scores_b)
+
+
+def randomise_test(test: Callable[[Partition, Partition], Scores]) -> Scores:
+    def intern(m):
+        for _ in range(10):
+            gold = beta_partition(m, 1, 1)
+            sys = beta_partition(m, 1, 1)
+            yield test(gold, sys)
+    mentions = list(range(100))
+    return Scores.average(intern(mentions))
+
+
+
+# print(golds_vs_entity(iter_ancor()))
 
 
 ## exemple blanc
-# k = [{1}, {2}, {3}, {4}, {5, 12, 14}, {6}, {7, 9}, {8}, {10}, {11}, {13}]
-# r1 = [{1}, {2}, {3}, {4, 6}, {5, 12}, {7, 9, 14}, {8}, {10}, {11}, {13}]
+k = [{1}, {2}, {3}, {4}, {5, 12, 14}, {6}, {7, 9}, {8}, {10}, {11}, {13}]
+r = [{1}, {2}, {3}, {4, 6}, {5, 12}, {7, 9, 14}, {8}, {10}, {11}, {13}]
 # r2 = [{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}]
 # r3 = [{1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}, {12}, {13}, {14}]
 #
 # R = [r1]  # [r1, r2, r3]
+
+
+##  exemple lea
+# k = [{'a', 'b', 'c'}, {'d', 'e', 'f', 'g'}]
+# r = [{'a', 'b'}, {'c', 'd'},{'f', 'g', 'h', 'i'}]
+
+#
+# print(evaluate(k, r))
+
+## exemple à la mano
+
+#k = [{1}, {2, 3}, {4, 5, 6}, {13, 14}]
+#r = [{1}, {3}, {2, 4, 5, 6}, {13, 14}]
+
+# print(evaluate(k,r))
+#print(scale_test(k, r))
+#print(symetry_test(k, r))
+# print(evaluate(k,r))
+
+#print(randomise_test(scale_test))
+#print(randomise_test(symetry_test))
+
+#print(reduce(lambda x, y: x+y, (len(beta_partition(list(range(100)), 1, 1)) for _ in range(100)))/100)
+
+dic = {}
+for partition in iter_ancor():
+    for cluster in partition:
+        dic.setdefault(len(cluster), 0)
+        dic[len(cluster)] +=1
+
+for k, v in sorted(dic.items()):
+    print(k,v)
